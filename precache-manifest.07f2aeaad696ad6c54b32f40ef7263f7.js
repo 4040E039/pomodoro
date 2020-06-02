33fab10f7b739983f8bc5760481bc4e98006737d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-02 06:49:32 +0000

    Deploy 4040E039/pomodoro to github.com/4040E039/pomodoro.git:gh-pages

---
+++ precache-manifest.07f2aeaad696ad6c54b32f40ef7263f7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4777654d9b29be71c5e90af6a45f96d",
+    "url": "alarms/alarm1.mp3"
+  },
+  {
+    "revision": "1bc4e90fc20ac86a34979d0383c77490",
+    "url": "alarms/alarm2.mp3"
+  },
+  {
+    "revision": "8e43441c9c590d40566e425908551ebf",
+    "url": "alarms/alarm3.mp3"
+  },
+  {
+    "revision": "a5e0efdf83a459396a44",
+    "url": "css/app.3e88471f.css"
+  },
+  {
+    "revision": "7dd71af5347ca0feb5cb",
+    "url": "css/chunk-vendors.3ed330a7.css"
+  },
+  {
+    "revision": "e6fd70a48c5f566008f82b6c36d2500e",
+    "url": "index.html"
+  },
+  {
+    "revision": "a5e0efdf83a459396a44",
+    "url": "js/app.b036c511.js"
+  },
+  {
+    "revision": "7dd71af5347ca0feb5cb",
+    "url": "js/chunk-vendors.ed685797.js"
+  },
+  {
+    "revision": "b69e655d8b512dca7ed6",
+    "url": "js/history.f41ebf0b.js"
+  },
+  {
+    "revision": "e49e438c2e016485b6f7",
+    "url": "js/list.9a333978.js"
+  },
+  {
+    "revision": "e811ed583b3647012f35",
+    "url": "js/notFound.cbb21fd7.js"
+  },
+  {
+    "revision": "f39fd44500ac4d5282f6",
+    "url": "js/settings.a823e796.js"
+  },
+  {
+    "revision": "3dc210960fe7e4127773465ac5d8a550",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
